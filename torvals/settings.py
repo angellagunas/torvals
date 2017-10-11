98516f8645a4dcfe1a9f774a09eef47f056fe19b
Angel Lagunas <angel.lagunas@ironbit.com.mx>
2017-10-11 18:34:27 -0500

    feat: add project structure

--- torvals/settings.py
+++ torvals/settings.py
@@ -39,6 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'drf_scaffolding',
+    'soft_drf'
+]
+
+LOCAL_APPS = []
+
+INSTALLED_APPS += THIRD_PARTY_APPS + LOCAL_APPS
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -118,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SHOW_DOCUMENTATION = True
+TITLE_DOCUMENTATION = "Torvals Project"
