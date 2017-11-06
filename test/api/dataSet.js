5eccbbd8f9afb5d7d197af06b8a3e9427ba03c4a
Jonathan Corona <soequelletindolu@gmail.com>
2017-11-06 13:27:55 -0600

    API Endpoint for delete admin and app

--- test/api/dataSet.js
+++ test/api/dataSet.js
@@ -16,18 +16,17 @@ describe('/datasets', () => {
   const password = '1234'
 
   beforeEach(async function () {
-    //await clearDatabase()
+    // await clearDatabase()
   })
 
   describe.only('[post] / Create dataSets', () => {
-    
     it('should return a 200 then the dataset created', async function () {
       await clearDatabase()
       const user = await createUser({ password })
       const org = await Organization.create({
-          name: 'Una org',
-          description: 'Una descripción'
-        })
+        name: 'Una org',
+        description: 'Una descripción'
+      })
 
       const res = await test()
         .post('/api/admin/datasets')
@@ -39,33 +38,41 @@ describe('/datasets', () => {
         })
         .set('Accept', 'application/json')
         .expect(200)
-      
+
       const newDataSet = await DataSet.findOne({'uuid': res.body.data.uuid})
       expect(newDataSet.name).equal('Un dataset')
       expect(newDataSet.description).equal('Una descripción')
-    
     })
 
     describe('[get] / Obtain dataSets', () => {
-    it('should return a 200', async function () {
-      await test()
+      it('should return a 200', async function () {
+        await test()
         .get('/api/admin/datasets')
         .set('Accept', 'application/json')
         .expect(200)
-    })
+      })
 
-    it('should return list of datasets', async function () {
-      const res = await test()
+      it('should return list of datasets', async function () {
+        const res = await test()
         .get('/api/admin/datasets')
         .set('Accept', 'application/json')
         .expect(200)
 
-        const datasets = await DataSet.findOne({name:'Un dataset'})
+        const datasets = await DataSet.findOne({name: 'Un dataset'})
         expect(datasets.name).equal('Un dataset')
+      })
+    })
+
+    describe('[delete] / Soft Delete dataSet', () => {
+      it('should return true for deleted', async function () {
+        const datasets = await DataSet.findOne({name: 'Un dataset'})
+        const res = await test()
+        .delete('/api/admin/datasets/' + datasets.uuid)
+        .set('Accept', 'application/json')
+        .expect(200)
+
+        expect(res.body.data.isDeleted).equal(true)
+      })
     })
   })
-
-  })
-  
-
 })
