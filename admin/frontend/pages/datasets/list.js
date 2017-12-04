3ec29d4ab6854f59ac34f96d8f86ed0fc9ffe0a2
Luis Lizarraga <siul.al4@gmail.com>
2017-12-04 13:04:58 -0600

    Adding ListPage to datasets list, removing logs

--- admin/frontend/pages/datasets/list.js
+++ admin/frontend/pages/datasets/list.js
@@ -1,112 +1,24 @@
-import React, { Component } from 'react'
-import { branch } from 'baobab-react/higher-order'
-import PropTypes from 'baobab-react/prop-types'
+import React from 'react'
 import Link from '~base/router/link'
 import api from '~base/api'
 
-import Page from '~base/page'
+import ListPage from '~base/list-page'
 import {loggedIn} from '~base/middlewares/'
-import { BranchedPaginatedTable } from '~base/components/base-paginatedTable'
 import CreateDataSet from './create'
-import BaseFilterPanel from '~base/components/base-filters'
-import FontAwesome from 'react-fontawesome'
 
-const schema = {
-  type: 'object',
-  required: [],
-  properties: {
-    name: {type: 'text', title: 'Por nombre'},
-    organization: {type: 'text', title: 'Por organización'},
-    status: {
-      type: 'text',
-      title: 'Por status',
-      values: [
-        {
-          name: 'new',
-          uuid: 'new'
-        },
-        {
-          name: 'uploading',
-          uuid: 'uploading'
-        },
-        {
-          name: 'uploaded',
-          uuid: 'uploaded'
-        },
-        {
-          name: 'preprocessing',
-          uuid: 'preprocessing'
-        },
-        {
-          name: 'configuring',
-          uuid: 'configuring'
-        },
-        {
-          name: 'processing',
-          uuid: 'processing'
-        },
-        {
-          name: 'reviewing',
-          uuid: 'reviewing'
-        },
-        {
-          name: 'ready',
-          uuid: 'ready'
-        }
-      ]
-    }
-  }
-}
-
-const uiSchema = {
-  name: {'ui:widget': 'SearchFilter'},
-  organization: {'ui:widget': 'SelectSearchFilter'},
-  status: {'ui:widget': 'SelectSearchFilter'}
-}
-
-class DataSets extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      className: '',
-      orgs: [],
-      filters: {}
-    }
-
-    this.toggleFilterPanel = this.toggleFilterPanel.bind(this)
-    this.handleOnFilter = this.handleOnFilter.bind(this)
-  }
-
-  componentWillMount () {
-    this.context.tree.set('datasets', {
-      page: 1,
-      totalItems: 0,
-      items: [],
-      pageLength: 10
-    })
-    this.context.tree.commit()
-    this.loadOrgs()
-  }
-
-  async loadOrgs () {
-    var url = '/admin/organizations/'
-    const body = await api.get(
-      url,
-      {
-        start: 0,
-        limit: 0
-      }
-    )
-
-    this.setState({
-      ...this.state,
-      orgs: body.data
-    })
-
-    schema.properties.organization['values'] = body.data
-  }
-
-  getColumns () {
+export default ListPage({
+  path: '/datasets',
+  title: 'Datasets',
+  icon: 'check',
+  exact: true,
+  validate: loggedIn,
+  titleSingular: 'Dataset',
+  create: true,
+  createComponent: CreateDataSet,
+  baseUrl: '/admin/datasets',
+  branchName: 'datasets',
+  detailUrl: '/admin/datasets/detail/',
+  getColumns: () => {
     return [
       {
         'title': 'Name',
@@ -152,129 +64,71 @@ class DataSets extends Component {
         }
       }
     ]
-  }
-
-  toggleFilterPanel (isFilterOpen) {
-    this.setState({isFilterOpen: !isFilterOpen})
-  }
-
-  handleOnFilter (formData) {
-    let filters = {}
-
-    for (var field in formData) {
-      if (formData[field]) {
-        filters[field] = formData[field]
+  },
+  filters: true,
+  schema: {
+    type: 'object',
+    required: [],
+    properties: {
+      name: {type: 'text', title: 'Por nombre'},
+      organization: {type: 'text', title: 'Por organización'},
+      status: {
+        type: 'text',
+        title: 'Por status',
+        values: [
+          {
+            name: 'new',
+            uuid: 'new'
+          },
+          {
+            name: 'uploading',
+            uuid: 'uploading'
+          },
+          {
+            name: 'uploaded',
+            uuid: 'uploaded'
+          },
+          {
+            name: 'preprocessing',
+            uuid: 'preprocessing'
+          },
+          {
+            name: 'configuring',
+            uuid: 'configuring'
+          },
+          {
+            name: 'processing',
+            uuid: 'processing'
+          },
+          {
+            name: 'reviewing',
+            uuid: 'reviewing'
+          },
+          {
+            name: 'ready',
+            uuid: 'ready'
+          }
+        ]
       }
     }
-    this.setState({filters})
-  }
-
-  showModal () {
-    this.setState({
-      className: ' is-active'
-    })
-  }
-
-  hideModal () {
-    this.setState({
-      className: ''
-    })
-  }
-
-  finishUp (object) {
-    this.setState({
-      className: ''
-    })
-    this.props.history.push('/admin/datasets/detail/' + object.uuid)
-  }
-
-  render () {
-    let { isFilterOpen, filters } = this.state
-    let filterPanel
-
-    if (isFilterOpen) {
-      filterPanel = (
-        <div className='column is-narrow side-filters is-paddingless'>
-          <BaseFilterPanel
-            schema={schema}
-            uiSchema={uiSchema}
-            filters={filters}
-            onFilter={this.handleOnFilter}
-            onToggle={() => this.toggleFilterPanel(isFilterOpen)} />
-        </div>
-      )
-    }
-
-    if (!isFilterOpen) {
-      filterPanel = (<div className='searchbox'>
-        <a
-          href='javascript:void(0)'
-          className='card-header-icon has-text-white'
-          aria-label='more options'
-          onClick={() => this.toggleFilterPanel(isFilterOpen)}
-        >
-          <FontAwesome name='search' />
-        </a>
-      </div>)
-    }
-
-    return (
-      <div className='columns c-flex-1 is-marginless'>
-        <div className='column is-paddingless'>
-          <div className='section is-paddingless-top'>
-            <h1 className='is-size-3 is-padding-top-small is-padding-bottom-small'>DataSets</h1>
-            <div className='card'>
-              <header className='card-header'>
-                <p className='card-header-title'>
-                    DataSets
-                </p>
-                <div className='card-header-select'>
-                  <button className='button is-primary' onClick={() => this.showModal()}>
-                    New DataSet
-                  </button>
-                  <CreateDataSet
-                    className={this.state.className}
-                    hideModal={this.hideModal.bind(this)}
-                    finishUp={this.finishUp.bind(this)}
-                    branchName='datasets'
-                    baseUrl='/admin/datasets'
-                    url='/admin/datasets'
-                  />
-
-                </div>
-              </header>
-              <div className='card-content'>
-                <div className='columns'>
-                  <div className='column'>
-                    <BranchedPaginatedTable
-                      branchName='datasets'
-                      baseUrl='/admin/datasets'
-                      filters={this.state.filters}
-                      columns={this.getColumns()}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        { filterPanel }
-      </div>
+  },
+  uiSchema: {
+    name: {'ui:widget': 'SearchFilter'},
+    organization: {'ui:widget': 'SelectSearchFilter'},
+    status: {'ui:widget': 'SelectSearchFilter'}
+  },
+  loadValues: async function () {
+    var url = '/admin/organizations/'
+    const body = await api.get(
+      url,
+      {
+        start: 0,
+        limit: 0
+      }
     )
+
+    return {
+      'organization': body.data
+    }
   }
-}
-
-DataSets.contextTypes = {
-  tree: PropTypes.baobab
-}
-
-const branchedDataSets = branch({datasets: 'datasets'}, DataSets)
-
-export default Page({
-  path: '/datasets',
-  title: 'Datasets',
-  icon: 'check',
-  exact: true,
-  validate: loggedIn,
-  component: branchedDataSets
 })
