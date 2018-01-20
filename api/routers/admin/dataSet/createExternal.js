09cc58406d52dcf4a729b6fc845a9c01648acead
Luis Lizarraga <siul.al4@gmail.com>
2018-01-19 18:56:18 -0600

    Added createExternal endpoint for Datasets

---
+++ api/routers/admin/dataSet/createExternal.js
@@ -0,0 +1,75 @@
+const Route = require('lib/router/route')
+const lov = require('lov')
+
+const { DataSet, Project } = require('models')
+const Api = require('lib/abraxas/api')
+const request = require('lib/request')
+
+module.exports = new Route({
+  method: 'post',
+  path: '/addExternal',
+  validator: lov.object().keys({
+    uuid: lov.string().required(),
+    project: lov.string().required()
+  }),
+  handler: async function (ctx) {
+    const body = ctx.request.body
+    let project, org
+
+    project = await Project.findOne({uuid: body.project}).populate('organization')
+
+    if (!project) {
+      ctx.throw(404, 'Project not found')
+    }
+
+    org = project.organization
+
+    const dataset = await DataSet.create({
+      name: body.name || 'New External',
+      description: body.description,
+      organization: org._id,
+      createdBy: ctx.state.user,
+      project: project._id,
+      externalId: body.uuid
+    })
+
+    if (project) {
+      project.datasets.push({
+        dataset: dataset,
+        columns: []
+      })
+
+      await project.save()
+    }
+
+    var apiData = Api.get()
+    if (!apiData.token) {
+      await Api.fetch()
+      apiData = Api.get()
+    }
+
+    var options = {
+      url: `${apiData.hostname}${apiData.baseUrl}/datasets/${dataset.externalId}`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${apiData.token}`
+      },
+      json: true,
+      persist: true
+    }
+
+    try {
+      var res = await request(options)
+
+      dataset.process(res)
+    } catch (e) {
+      ctx.throw(401, 'Failed to send Dataset for conciliation')
+    }
+
+    ctx.body = {
+      data: dataset
+    }
+  }
+})
