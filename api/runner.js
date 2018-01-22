37ae078a81a60415770c783cb7f8965c194f9e96
Luis Lizarraga <siul.al4@gmail.com>
2018-01-22 14:26:24 -0600

    Adding multiple workers to API

--- api/runner.js
+++ api/runner.js
@@ -2,7 +2,28 @@ require('../config')
 require('lib/databases/mongo')
 
 const { apiPort } = require('config/server')
+const { multiple, numWorkers } = require('config/multiple-workers')
 const app = require('./')
 
-app.listen(apiPort)
-console.log(`Api started on port ${apiPort}`)
+if (multiple) {
+  const cluster = require('cluster')
+
+  if (cluster.isMaster) {
+    // Master:
+    // Let's fork as many workers as you have logical CPU cores
+
+    for (var i = 0; i < numWorkers; ++i) {
+      cluster.fork()
+    }
+    console.log(`Api started on port ${apiPort}, ${numWorkers} workers`)
+  } else {
+    // Worker:
+    // (Workers can share any TCP connection.
+    //  In this case its a HTTP server)
+
+    app.listen(apiPort)
+  }
+} else {
+  app.listen(apiPort)
+  console.log(`Api started on port ${apiPort}`)
+}
