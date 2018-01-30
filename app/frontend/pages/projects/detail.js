6f05f8cb6dd5cbbee6d0e206f1916d1da473c102
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-01-30 13:12:21 -0600

    Historical View

--- app/frontend/pages/projects/detail.js
+++ app/frontend/pages/projects/detail.js
@@ -10,6 +10,7 @@ import Loader from '~base/components/spinner'
 import ProjectForm from './create-form'
 import Tabs from '~base/components/base-tabs'
 import TabDatasets from './detail-tabs/tab-datasets'
+import {Line} from 'react-chartjs-2'
 
 class ProjectDetail extends Component {
   constructor (props) {
@@ -45,6 +46,165 @@ class ProjectDetail extends Component {
 
   render () {
     const { project } = this.state
+
+    const data = {
+      labels: ['01/11/2017', '01/12/2017', '01/01/2018', '01/02/2018', '01/02/2018', '01/03/2018', '01/04/2018'],
+      datasets: [
+        {
+          label: 'Predicción',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#01579B',
+          borderColor: '#01579B',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [200000, 255888, 254888, 205888, 200000, 400000, 350000]
+        },
+        {
+          label: 'Ajuste',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#FF9800',
+          borderColor: '#FF9800',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [150480, 200000, 404888, 165888, 190000, 480000, 120000]
+
+        },
+        {
+          label: 'Venta registrada',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#8BC34A',
+          borderColor: '#8BC34A',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [160480, 160000, 254888, 205888, 190000, 300000, 280000]
+        }
+
+      ]
+    }
+    var historical = (<div className='card'>
+      <div className='card-content'>
+        <div className='columns'>
+          <div className='column is-half' />
+          <div className='column'>
+            <div className='card'>
+              <div className='card-header'>
+                <h1 className='card-header-title'>Totales de Venta</h1>
+              </div>
+              <div className='card-content historical-container'>
+                <table className='table historical'>
+                  <thead>
+                    <tr>
+                      <th colSpan='2'>Predicción</th>
+                      <th colSpan='2'>Ajuste</th>
+                      <th colSpan='2'>Venta Registrada</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className='font-blue'>
+                        Semana 19
+                      </td>
+                      <td className='font-blue'>
+                        $ 2,684,262.60
+                      </td>
+                      <td className='font-orange'>
+                        Semana 19
+                      </td>
+                      <td className='font-orange'>
+                        $ 2,684,262.60
+                      </td>
+                      <td className='font-green'>
+                        Semana 19
+                      </td>
+                      <td className='font-green'>
+                        $ 2,684,262.60
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className='font-blue'>
+                        Semana 19
+                      </td>
+                      <td className='font-blue'>
+                        $ 2,684,262.60
+                      </td>
+                      <td className='font-orange'>
+                        Semana 19
+                      </td>
+                      <td className='font-orange'>
+                        $ 2,684,262.60
+                      </td>
+                      <td className='font-green'>
+                        Semana 19
+                      </td>
+                      <td className='font-green'>
+                        $ 2,684,262.60
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className='font-blue'>
+                        Semana 19
+                      </td>
+                      <td className='font-blue'>
+                        $ 2,684,262.60
+                      </td>
+                      <td className='font-orange'>
+                        Semana 19
+                      </td>
+                      <td className='font-orange'>
+                        $ 2,684,262.60
+                      </td>
+                      <td className='font-green'>
+                        Semana 19
+                      </td>
+                      <td className='font-green'>
+                        $ 2,684,262.60
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Line data={data} width={200} height={50} />
+    </div>)
 
     if (!this.state.loaded) {
       return <Loader />
@@ -92,10 +252,10 @@ class ProjectDetail extends Component {
         content: <div className='card'>Ajustes</div>
       },
       {
-        name: 'Historial',
-        title: 'Historial',
+        name: 'Historico',
+        title: 'Historico',
         icon: 'fa-history',
-        content: <div className='card'>Historial</div>
+        content: historical
       }
 
     ]
