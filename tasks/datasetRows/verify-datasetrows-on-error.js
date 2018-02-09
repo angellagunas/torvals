6f6642b5d51190bfe0e783745e3a64a639fb6571
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-02-09 17:56:48 -0600

    Refactor

--- tasks/datasetRows/verify-datasetrows-on-error.js
+++ tasks/datasetRows/verify-datasetrows-on-error.js
@@ -2,10 +2,9 @@
 require('../../config')
 require('lib/databases/mongo')
 
-const Api = require('lib/abraxas/api')
 const Task = require('lib/task')
 const { DataSetRow } = require('models')
-const request = require('lib/request')
+const verifyDatasetrows = require('queues/update-datasetrows')
 
 const task = new Task(async function (argv) {
   console.log('Fetching DatasetsRows...')
@@ -19,46 +18,10 @@ const task = new Task(async function (argv) {
     return true
   }
 
-  console.log('Obtaining Abraxas API token ...')
-  await Api.fetch()
-  const apiData = Api.get()
-
   for (var datasetRow of datasetRows) {
-    var options = {
-      url: `${apiData.hostname}${apiData.baseUrl}/datasets/${datasetRow.dataset.externalId}`,
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${apiData.token}`,
-        'If-Match': `${datasetRow.dataset.etag}`
-      },
-      body: {
-        data_rows_id: datasetRow.externalId,
-        adjustment: datasetRow.data.adjustment
-      },
-      json: true,
-      persist: true
-    }
-    var dataset = datasetRow.dataset
-    console.log(`Sending adjustment of DataSetRow ${datasetRow.externalId}`)
-
-    try {
-      var res = await request(options)
-
-      if (res._status === 'OK') {
-        dataset.etag = res._etag
-        datasetRow.set({status: 'adjusted'})
-        console.log(`Sent succesfully adjustment of DataSetRow ${datasetRow.externalId}`)
-      } else {
-        datasetRow.set({status: 'error'})
-      }
-    } catch (e) {
-      datasetRow.set({status: 'error'})
-    }
-
-    await dataset.save()
+    datasetRow.set({status: 'sendingChanges'})
     await datasetRow.save()
+    verifyDatasetrows.add({uuid: datasetRow.uuid})
   }
 })
 
