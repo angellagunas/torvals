074b8494f4f28ba4758fe897f5a6b42c120b1c9c
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-02-13 11:19:35 -0600

    Remove comments

--- api/routers/admin/channel/list.js
+++ api/routers/admin/channel/list.js
@@ -7,32 +7,6 @@ module.exports = new Route({
   method: 'get',
   path: '/',
   handler: async function (ctx) {
-    // var filters = {}
-    //
-    // for (var filter in ctx.request.query) {
-    //   if (filter === 'limit' || filter === 'start' || filter === 'sort') {
-    //     continue
-    //   }
-    //   if (filter === 'organization') {
-    //     const organization = await Organization.findOne(
-    //       {'uuid': ctx.request.query[filter]}
-    //     )
-    //
-    //     if (organization) {
-    //       filters['organization'] = ObjectId(organization._id)
-    //     }
-    //     continue
-    //   }
-    //   filters[filter] = { '$regex': ctx.request.query[filter], '$options': 'i' }
-    // }
-    // var channels = await Channel.dataTables({
-    //   limit: ctx.request.query.limit || 20,
-    //   skip: ctx.request.query.start,
-    //   find: {...filters, isDeleted: false},
-    //   sort: ctx.request.query.sort || '-dateCreated',
-    //   populate: 'organization'
-    // })
-
     var sortStatement = {}
 
     var columns = [
