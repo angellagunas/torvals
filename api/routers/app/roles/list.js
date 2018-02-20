c0b9389b4fdaa1a79aed5d0d72c6df457a8f048a
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-02-20 12:21:13 -0600

    Sort priority on app

--- api/routers/app/roles/list.js
+++ api/routers/app/roles/list.js
@@ -22,7 +22,7 @@ module.exports = new Route({
       limit: ctx.request.query.limit || 20,
       skip: ctx.request.query.start,
       find: {isDeleted: false, ...filters},
-      sort: ctx.request.query.sort || '-dateCreated'
+      sort: 'priority'
     })
 
     ctx.body = role
