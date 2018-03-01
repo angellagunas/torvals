a979e8002fec306ce466c74b1e544f6a1e3202f6
Luis Lizarraga <siul.al4@gmail.com>
2018-03-01 12:27:50 -0600

    Changing aws region, using s3.

--- models/user.js
+++ models/user.js
@@ -215,10 +215,10 @@ userSchema.methods.uploadProfilePicture = async function (file) {
 
 userSchema.virtual('profileUrl').get(function () {
   if (this.profilePicture && this.profilePicture.url) {
-    return 'https://s3-' + this.profilePicture.region + '.amazonaws.com/' + this.profilePicture.bucket + '/' + this.profilePicture.url
+    return 'https://s3.' + this.profilePicture.region + '.amazonaws.com/' + this.profilePicture.bucket + '/' + this.profilePicture.url
   }
 
-  return 'https://s3-us-west-2.amazonaws.com/pythia-kore-dev/avatars/default.jpg'
+  return 'https://s3.us-west-2.amazonaws.com/pythia-kore-dev/avatars/default.jpg'
 })
 
 userSchema.methods.validatePassword = async function (password) {
--- runner.js
+++ runner.js
@@ -18,19 +18,21 @@ console.log(`App started on port ${adminPort}`)
 
 // Crons
 const checkDatasets = require('crons/check-datasets')
-checkDatasets.run()
+checkDatasets.schedule()
 
 const checkProjects = require('crons/check-projects')
-checkProjects.run()
+checkProjects.schedule()
 
 const checkDatasetRows = require('crons/check-datasetsrows')
-checkDatasetRows.run()
+checkDatasetRows.schedule()
 
 // Queue
 const finishUpload = require('queues/finish-upload')
 finishUpload.run()
+finishUpload.setCliLogger()
 finishUpload.setCleanUp()
 
 const updateDatasetRows = require('queues/update-datasetrows')
 updateDatasetRows.run()
+updateDatasetRows.setCliLogger()
 updateDatasetRows.setCleanUp()
