55152b3b7bb7a305fc7f4e1a34decb2056ef6165
devil-geek <devil.prowler@gmail.com>
2018-03-06 15:02:28 -0600

    Removed useless transform

--- admin/frontend/pages/carousel.js
+++ admin/frontend/pages/carousel.js
@@ -5,23 +5,6 @@ const CarouselContainer = styled.div`
   display: flex;
   margin: 0 0 20px 20px;
   transition: ${(props) => props.sliding ? 'none' : 'transform 1s ease'};
-  /* transform: ${(props) => {
-    
-    if (props.numSlides === 1) return 'translateX(0%)'
-    if (props.numSlides === 2) {
-      if (!props.sliding && props.direction === 'next') return 'translateX(calc(-50% + 20px))'
-      if (!props.sliding && props.direction === 'prev') return 'translateX(0%)'
-      if (props.direction === 'prev') 
-      return 'translateX(calc(-50% + 20px))'
-      return 'translateX(0%)'
-    }
-    /* if (!props.sliding) 
-      return 'translateX(calc(-80% - 20px))'
-    if (props.direction === 'prev') 
-      return 'translateX(calc(2 * (-80% - 20px)))'
-    
-      return 'translateX(calc(0%))' */
-  }}; */
 `
 
 const Wrapper = styled.div`
