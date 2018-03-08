dcc863d223ccdec5db4c308fe4a6bd5c3a277d19
Unknown <csarg26@gmail.com>
2018-03-08 17:38:22 -0600

    Queue  prices by organization

--- tasks/prices/get-prices.js
+++ tasks/prices/get-prices.js
@@ -1,13 +1,19 @@
-// node tasks/prices/get-prices.js
+// node tasks/prices/get-prices.js --uuid uuid
+// uuid organization
 require('../../config')
 require('lib/databases/mongo')
 
 const Api = require('lib/abraxas/api')
 const Task = require('lib/task')
-const { Price, Product, Channel } = require('models')
+const { Price, Product, Channel, Organization } = require('models')
 const request = require('lib/request')
 
 const task = new Task(async function (argv) {
+  const organization = await Organization.findOne({uuid: argv.uuid})
+  if (!organization) {
+    console.log('Error Organization not found')
+    return false
+  }
   console.log('Fetching Prices ...')
 
   console.log('Obtaining Abraxas API token ...')
@@ -32,7 +38,14 @@ const task = new Task(async function (argv) {
   for (var p of res._items) {
     var price = await Price.findOne({externalId: p._id})
     var product = await Product.findOne({externalId: p.producto_id})
-    if (!product) { product = {_id: null} }
+    if (!product) {
+      product = await Product.create({
+        name: 'Not identified',
+        externalId: p._id,
+        organization: organization._id,
+        isNewExternal: true
+      })
+    }
     var channel = await Channel.findOne({externalId: p.canal_id})
     if (!channel) { channel = {_id: null} }
 
@@ -44,6 +57,7 @@ const task = new Task(async function (argv) {
         productExternalId: p.producto_id,
         channel: channel._id,
         channelExternalId: p.canal_id,
+        organization: organization._id,
         etag: p._etag
       })
     } else {
@@ -54,6 +68,7 @@ const task = new Task(async function (argv) {
         productExternalId: p.producto_id,
         channel: channel._id,
         channelExternalId: p.canal_id,
+        organization: organization._id,
         etag: p._etag
       })
       await price.save()
