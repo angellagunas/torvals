365286e24d9a0b5f229a5a491355a3a2813d7829
Unknown <csarg26@gmail.com>
2018-03-16 14:27:47 -0600

    Removed check prices from crons

--- crons/index.js
+++ crons/index.js
@@ -1,12 +1,11 @@
 const checkDatasets = require('./check-datasets')
 const checkProjects = require('./check-projects')
 const checkDatasetRows = require('./check-datasetsrows')
-const checkPrices = require('./check-prices')
 // #Requires
 
 module.exports = {
   'check-datasets': checkDatasets,
   'check-datasetrows': checkDatasetRows,
-  'check-projects': checkProjects, // #Exports
-  'check-prices': checkPrices
+  'check-projects': checkProjects // #Exports
+
 }
