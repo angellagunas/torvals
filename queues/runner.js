8127c7f8b6a3c30a87d417aed09007f5f6babb16
Luis Lizarraga <siul.al4@gmail.com>
2018-03-20 12:20:12 -0600

    Fixing main runner, queue runner

--- queues/runner.js
+++ queues/runner.js
@@ -1,4 +1,8 @@
 const queues = require('./')
 const { each } = require('lodash')
 
-each(queues, queue => { queue.run() })
+each(queues, queue => {
+  queue.run()
+  queue.setCliLogger()
+  queue.setCleanUp()
+})
--- runner.js
+++ runner.js
@@ -1,36 +1,22 @@
 const config = require('./config')
 require('lib/databases/mongo')
 
-const { apiPort, appPort, adminPort } = require('config/server')
-const app = require('./app')
+const { apiPort } = require('config/server')
 const api = require('./api')
-const admin = require('./admin')
 
 // Web services
 api.listen(apiPort)
 console.log(`Api started on port ${apiPort}`)
 
-app.listen(appPort)
-console.log(`App started on port ${appPort}`)
-
-admin.listen(adminPort)
-console.log(`App started on port ${adminPort}`)
-
 // Crons
-const checkDatasets = require('crons/check-datasets')
-checkDatasets.run()
-
-const checkProjects = require('crons/check-projects')
-checkProjects.run()
-
-const checkDatasetRows = require('crons/check-datasetsrows')
-checkDatasetRows.run()
+const crons = require('crons/')
+const { each } = require('lodash')
+each(crons, cron => cron.schedule())
 
 // Queue
-const finishUpload = require('queues/finish-upload')
-finishUpload.run()
-finishUpload.setCleanUp()
-
-const updateDatasetRows = require('queues/update-datasetrows')
-updateDatasetRows.run()
-updateDatasetRows.setCleanUp()
+const queues = require('queues/')
+each(queues, queue => {
+  queue.run()
+  queue.setCliLogger()
+  queue.setCleanUp()
+})
