1bb42e79a92cc1c5b9963d80573caa8fd77cb76a
Cesar <csarg26@gmail.com>
2018-04-12 15:52:33 -0500

    Change projects organization

--- api/routers/admin/project/update.js
+++ api/routers/admin/project/update.js
@@ -1,7 +1,7 @@
 const Route = require('lib/router/route')
 const lov = require('lov')
 
-const {Project, Organization, DataSetRow, Product, AdjustmentRequest} = require('models')
+const {Project, Organization, DataSetRow, Product, AdjustmentRequest, SalesCenter, Channel} = require('models')
 
 module.exports = new Route({
   method: 'post',
@@ -30,23 +30,62 @@ module.exports = new Route({
         await dataset.processData()
       }
       var datasetrows = await DataSetRow.find({'project': project._id})
-      for (var dsr of datasetrows) {
-        dsr.organization = data.organization._id
+      for (var datasetrow of datasetrows) {
+        datasetrow.organization = data.organization._id
 
-        let product = await Product.findOne({'_id': dsr.product})
-        if (product) {
-          let productNewOrg = await Product.findOne({'externalId': product.externalId})
-          if (productNewOrg) {
-            dsr.product = productNewOrg._id
-          } else {
-            // TODO: create new product with the same info different organization
-            /* let newProduct = await Product.create({
-
-            }) */
-          }
-
-          await dsr.save()
+        let product = await Product.findOne({'_id': datasetrow.product})
+        let productNewOrg = await Product.findOne({'externalId': product.externalId, 'organization': data.organization._id})
+        if (productNewOrg) {
+          datasetrow.product = productNewOrg._id
+        } else {
+          product.organization = data.organization._id
+          let newProduct = await Product.create({
+            name: product.name,
+            organization: data.organization._id,
+            price: product.price || null,
+            description: product.description || null,
+            type: product.type || null,
+            category: product.category || null,
+            subcategory: product.subcategory || null,
+            externalId: product.externalId
+          })
+          datasetrow.product = newProduct._id
         }
+
+        let salesCenter = await SalesCenter.findOne({'_id': datasetrow.salesCenter})
+        let SalesCenterNewOrg = await SalesCenter.findOne({'externalId': salesCenter.externalId, 'organization': data.organization._id})
+        if (SalesCenterNewOrg) {
+          datasetrow.salesCenter = SalesCenterNewOrg._id
+        } else {
+          salesCenter.organization = data.organization._id
+          let newSalesCenter = await SalesCenter.create({
+            name: salesCenter.name,
+            organization: data.organization._id,
+            description: salesCenter.description || null,
+            address: salesCenter.address || null,
+            brand: salesCenter.brand || null,
+            region: salesCenter.region || null,
+            type: salesCenter.type || null,
+            externalId: salesCenter.externalId
+          })
+          datasetrow.salesCenter = newSalesCenter._id
+        }
+
+        let channel = await Channel.findOne({'_id': datasetrow.channel})
+        let channelNewOrg = await Channel.findOne({'externalId': channel.externalId, 'organization': data.organization._id})
+        if (channelNewOrg) {
+          datasetrow.channel = channelNewOrg._id
+        } else {
+          channel.organization = data.channel._id
+          let newChannel = await Channel.create({
+            name: channel.name,
+            externalId: channel.externalId,
+            organization: data.organization._id
+          })
+          datasetrow.channel = newChannel._id
+        }
+
+        await datasetrow.save()
       }
       var adjustmentrequest = await AdjustmentRequest.find({'project': project._id})
       for (var ar of adjustmentrequest) {
