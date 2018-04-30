7dbf080a7310a8a28a176e208674d719e6794828
Luis Lizarraga <siul.al4@gmail.com>
2018-04-30 10:11:56 -0500

    Aggregate without previous_sale

--- api/routers/app/project/local-historical.js
+++ api/routers/app/project/local-historical.js
@@ -1,73 +1,32 @@
 const Route = require('lib/router/route')
-const { Project, SalesCenter, Channel, Product, DataSetRow } = require('models')
-const _ = require('lodash')
-const moment = require('moment')
-const lov = require('lov')
+const { Project, DataSetRow } = require('models')
 
 module.exports = new Route({
   method: 'get',
-  path: '/local/historical/:uuid',
+  path: '/local/historical',
   handler: async function (ctx) {
     var data = ctx.request.query
-    const project = await Project.findOne({uuid: ctx.params.uuid}).populate('activeDataset')
-    ctx.assert(project, 404, 'Proyecto no encontrado')
-    if (!project.activeDataset) {
-      ctx.throw(404, 'No hay DataSet activo para el proyecto')
-    }
 
-    var match = []
+    const projects = await Project.find({uuid: {$in: data.project}})
+    const datasets = projects.map(item => { return item.activeDataset })
 
-    match.push({
-      '$match': {
-        dataset: project.activeDataset._id
+    const key = {date: '$data.forecastDate'}
+
+    var match = [
+      {
+        '$match': {
+          dataset: { $in: datasets }
+        }
+      },
+      {
+        '$group': {
+          _id: key,
+          prediction: { $sum: '$data.prediction' },
+          adjustment: { $sum: '$data.adjustment' },
+          sale: { $sum: '$data.sale' }
+        }
       }
-    })
-
-    var dateColumn = project.activeDataset.getDateColumn() || {name: 'fecha'}
-
-    // match.push({
-    //   '$match': {
-    //     [dateColumn.name]: {
-    //       '$gte': moment(data.start_date),
-    //       '$lte': moment(data.end_date)
-    //     }
-    //   }
-    // })
-
-    const requestBody = {
-      date_start: data.start_date,
-      date_end: data.end_date
-    }
-
-    if (data.salesCenter) {
-      const agenciaName = project.activeDataset.getSalesCenterColumn() || {name: 'agencia_id'}
-      const salesCenter = await SalesCenter.findOne({uuid: data.salesCenter})
-      ctx.assert(salesCenter, 404, 'Centro de ventas no encontrado')
-      // match[agenciaName.name] = salesCenter._id
-    }
-
-    if (data.channel) {
-      const channelName = project.activeDataset.getChannelColumn() || {name: 'canal_id'}
-      const channel = await Channel.findOne({uuid: data.channel})
-      ctx.assert(channel, 404, 'Canal no encontrado')
-      // match[channelName.name] = channel._id
-    }
-
-    if (data.product) {
-      const productName = project.activeDataset.getProductColumn() || {name: 'producto_id'}
-      const product = await Product.findOne({uuid: data.product})
-      ctx.assert(product, 404, 'Producto no encontrado')
-      // match[productName.name] = product._id
-    }
-
-    console.log(match)
-    match.push({
-      '$group': {
-        _id: '$data.semanaBimbo',
-        prediction: { $sum: '$data.prediction' },
-        adjustment: { $sum: '$data.adjustment' }
-      }
-    })
+    ]
 
     var responseData = await DataSetRow.aggregate(match)
 
