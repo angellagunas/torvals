30689a596b91f88ef753942fe5c16f6b148583c1
Cesar <csarg26@gmail.com>
2018-04-30 16:45:28 -0500

    Create comparation project and graphic organization endpoints

---
+++ api/routers/app/dashboard/comparation-projects.js
@@ -0,0 +1,26 @@
+const ObjectId = require('mongodb').ObjectID
+const Route = require('lib/router/route')
+const Api = require('lib/abraxas/api')
+
+const { Project } = require('models')
+
+module.exports = new Route({
+  method: 'post',
+  path: '/comparation/projects/:uuid',
+  handler: async function (ctx) {
+    const uuid = ctx.params.uuid
+    var data = ctx.request.body
+
+    const project = await Project.findOne({'uuid': uuid})
+    ctx.assert(project, 404, 'Proyecto no encontrado')
+
+    const requestBody = {
+      date_start: data.date_start,
+      date_end: data.date_end
+    }
+
+    var responseData = await Api.comparationProject(project.externalId, requestBody)
+
+    ctx.body = responseData
+  }
+})
