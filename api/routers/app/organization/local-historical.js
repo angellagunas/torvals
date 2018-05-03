812107602e631a57a5320adb1bd2aa8ffe35e7a4
Cesar <csarg26@gmail.com>
2018-05-03 11:57:20 -0500

    Calculate Mape on the backend

--- api/routers/app/organization/local-historical.js
+++ api/routers/app/organization/local-historical.js
@@ -92,9 +92,13 @@ module.exports = new Route({
     var previousSaleDict = {}
     responseDataAux.map(item => { previousSaleDict[moment(item._id.date).format('YYYY-MM-DD')] = item })
 
+    var total_prediction = 0
+    var total_sale = 0
+
     responseData = responseData.map(item => {
       let previousDate = moment(item._id.date).subtract(1, 'years').format('YYYY-MM-DD')
-
+      if (item.prediction) { total_prediction += item.prediction }
+      if (item.sale) { total_sale += item.sale }
       return {
         date: item._id.date,
         prediction: item.prediction,
@@ -104,10 +108,13 @@ module.exports = new Route({
       }
     })
 
+    var mape = (total_sale - total_prediction) / total_sale
+
     ctx.set('Cache-Control', 'max-age=172800')
 
     ctx.body = {
-      data: responseData
+      data: responseData,
+      mape: mape
     }
   }
 })
