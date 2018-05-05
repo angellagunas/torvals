78741b105512c5014e86b4ae123145d6f7473db7
Luis Lizarraga <siul.al4@gmail.com>
2018-05-04 19:19:43 -0500

    better migration

--- tasks/migrations/parse-forecastDate-datasetrows.js
+++ tasks/migrations/parse-forecastDate-datasetrows.js
@@ -11,27 +11,37 @@ const task = new Task(async function (argv) {
   console.log('Fetching DatasetsRows...')
 
   const filters = {dataset: ObjectId('5ae7a6bc1f25560012c29f20')}
-  const pageSize = 5000
 
   const size = await DataSetRow.find(filters).count()
-  const pages = Math.ceil(size / pageSize)
 
   if (size === 0) {
     console.log('No datasetRows to verify ...')
     return true
   }
 
-  for (var i = 1; i <= pages; i++) {
-    console.log(`Modifying page ${i} of ${pages}`)
-    let datasetRows = await DataSetRow.find(filters).skip((i - 1) * pageSize).limit(pageSize)
-    for (var row of datasetRows) {
-      let data = row.data
-      data.forecastDate = moment.utc(row.apiData.fecha, 'YYYY-MM-DD')
+  const rows = await DataSetRow.find(filters).cursor()
+  var bulkOps = []
 
-      row.set({data: data})
-      await row.save()
+  for (let row = await rows.next(); row != null; row = await rows.next()) {
+    var newDate = moment.utc(row.apiData.fecha, 'YYYY-MM-DD')
+
+    bulkOps.push(
+      {
+        'updateOne': {
+          'filter': { '_id': row._id },
+          'update': { '$set': { 'data.forecastDate': newDate } }
+        }
+      }
+    )
+
+    if (bulkOps.length === 1000) {
+      console.log(`1000 ops ==> ${moment().format()}`)
+      await DataSetRow.bulkWrite(bulkOps)
+      bulkOps = []
     }
   }
+
+  if (bulkOps.length > 0) await DataSetRow.bulkWrite(bulkOps)
 
   return true
 })
