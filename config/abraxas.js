68535f1e6b6478061f71f4c40cde3eb1a0e764fd
Cesar <csarg26@gmail.com>
2018-05-10 16:54:27 -0500

    Retreive headers from dataset csv. Configure columns and send it for processing to abraxas.

--- config/abraxas.js
+++ config/abraxas.js
@@ -2,5 +2,6 @@ module.exports = {
   abraxasHostname: process.env.ABRAXAS_HOSTNAME || '',
   abraxasBaseUrl: process.env.ABRAXAS_BASE_URL || '',
   abraxasUser: process.env.ABRAXAS_USER || '',
-  abraxasSecret: process.env.ABRAXAS_SECRET || ''
+  abraxasSecret: process.env.ABRAXAS_SECRET || '',
+  abraxasS3: process.env.S3_DATASET || true
 }
