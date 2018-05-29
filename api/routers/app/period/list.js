3b6cff6aecafaa78d6cf093d9dd064e3eb9871c9
Cesar <csarg26@gmail.com>
2018-05-29 17:25:14 -0500

    Created endpoints for list cycles and periods

--- api/routers/app/period/list.js
+++ api/routers/app/period/list.js
@@ -4,13 +4,13 @@ const {Organization, Period} = require('models')
 
 module.exports = new Route({
   method: 'get',
-  path: '/periods/:uuid',
+  path: '/:uuid',
   handler: async function (ctx) {
     var organizationId = ctx.params.uuid
 
-    /* if (organizationId !== ctx.state.organization.uuid) {
+    if (organizationId !== ctx.state.organization.uuid) {
       ctx.throw(404, 'Organización no encontrada')
-    } */
+    }
 
     const org = await Organization.findOne({'uuid': organizationId, 'isDeleted': false})
     ctx.assert(org, 404, 'Organización no encontrada')
