9ea547d3f2943db10944ba0eb7e56bf797001a49
Cesar <csarg26@gmail.com>
2018-06-04 19:25:58 -0500

    Adjustment filters with cycles

--- api/routers/app/datasetRow/filters.js
+++ api/routers/app/datasetRow/filters.js
@@ -8,7 +8,9 @@ const {
   SalesCenter,
   Product,
   AbraxasDate,
-  Role
+  Role,
+  Cycle,
+  Period
 } = require('models')
 
 module.exports = new Route({
@@ -51,22 +53,17 @@ module.exports = new Route({
     var products = await DataSetRow.find({isDeleted: false, dataset: dataset}).distinct('product')
     var channels = await DataSetRow.find({isDeleted: false, dataset: dataset}).distinct('channel')
     var salesCenters = await DataSetRow.find({isDeleted: false, dataset: dataset}).distinct('salesCenter')
-    var semanasBimbo = await DataSetRow.find({isDeleted: false, dataset: dataset}).distinct('data.semanaBimbo')
+    var cycles = await DataSetRow.find({isDeleted: false, dataset: dataset}).distinct('cycle')
 
-    semanasBimbo.sort((a, b) => {
-      return a - b
-    })
-
-    var dates = await AbraxasDate.find({
-      week: {$in: semanasBimbo},
+    var cycles = await Cycle.find({
+      organization: ctx.state.organization,
       dateStart: {$lte: moment.utc(dataset.dateMax), $gte: moment.utc(dataset.dateMin).subtract(1, 'days')}
     }).sort('-dateStart')
 
-    dates = dates.map(item => {
+    cycles = cycles.map(item => {
       return {
-        week: item.week,
-        month: item.month,
-        year: item.year,
+        cycle: item.cycle,
+        uuid: item.uuid,
         dateStart: item.dateStart,
         dateEnd: item.dateEnd
       }
@@ -77,11 +74,10 @@ module.exports = new Route({
     products = await Product.find({ _id: { $in: products } })
 
     ctx.body = {
-      semanasBimbo,
+      cycles,
       channels,
       salesCenters,
-      products,
-      dates
+      products
     }
   }
 })
