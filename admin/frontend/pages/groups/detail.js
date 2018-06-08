c6f228185263e5ddb55430a33bc0ee0ba96ac491
Rodman Swanston <swanston.rod@gmail.com>
2018-06-08 13:17:43 -0400

    Screen fixes

--- admin/frontend/pages/groups/detail.js
+++ admin/frontend/pages/groups/detail.js
@@ -339,7 +339,7 @@ class GroupDetail extends Component {
                     </p>
                     <div className='card-header-select'>
                       <button className='button is-primary' onClick={() => this.showModalList()}>
-                        Agregar usuario existente
+                        Agregar
                       </button>
                       <BaseModal
                         title='Usuarios para asignar'
