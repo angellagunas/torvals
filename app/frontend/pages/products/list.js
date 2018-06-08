94758035bc72d3e6bf23584d5457d3962a22b514
Luis Lizarraga <siul.al4@gmail.com>
2018-06-08 14:26:37 -0500

    Permissions for supervisor in front

--- app/frontend/pages/products/list.js
+++ app/frontend/pages/products/list.js
@@ -12,7 +12,7 @@ export default ListPage({
   title: 'Productos',
   icon: 'dropbox',
   exact: true,
-  roles: 'analyst, orgadmin, admin, manager-level-2, consultor',
+  roles: 'analyst, orgadmin, admin, manager-level-2, consultor, supervisor',
   validate: [loggedIn, verifyRole],
   titleSingular: 'Producto',
   create: true,
