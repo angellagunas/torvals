43ceacf961016e329ce55923d387932524b35d19
Cesar <csarg26@gmail.com>
2018-06-19 18:50:46 -0500

    Generate adjustment dataset files and upload them to aws

--- tasks/dataset/generate-download.js
+++ tasks/dataset/generate-download.js
@@ -1,32 +1,90 @@
-// node tasks/generate-download.js --uuid
+// node tasks/dataset/generate-download.js --uuid
 require('../../config')
 require('lib/databases/mongo')
 
 const Task = require('lib/task')
 const { AdjustmentDownload, DataSet, DataSetRow } = require('models')
-const { aws } = require('../config')
+const { aws } = require('../../config')
 const awsService = require('aws-sdk')
 const fs = require('fs-extra')
-const { v4 } = require('uuid')
 const path = require('path')
+const _ = require('lodash')
 
 const task = new Task(async function (argv) {
   console.log('Fetching Dataset...')
 
-  const dataset = await DataSet.findOne({uuid: argv.uuid, source: 'adjustment'})
+  const dataset = await DataSet.findOne({uuid: argv.uuid, source: 'adjustment'}).populate('rule')
 
   if (!dataset) {
     throw new Error('Dataset not found')
   }
 
-  const datasetRow = await DataSetRow.find({dataset: dataset})
-  var identifier = v4
-  const filePath = path.resolve('.', 'media', 'uploads', identifier)
+  const datasetRow = await DataSetRow.find({dataset: dataset}).populate('organization product catalogItems')
+  let fileName = dataset.uuid + '.csv'
+  const filePath = path.resolve('.', 'media', 'uploads', fileName)
   let writer = fs.createWriteStream(filePath)
-  for (let row of datasetRow) {
-    writer.write(row)
+  let i = 0
+  let catalogs = []
+  for (let catalog of dataset.rule.catalogs) {
+    if (i++) { writer.write(',') }
+    writer.write(catalog.name + '_id' + ',' + catalog.name)
+    catalogs.push(catalog.slug)
   }
-  console.log('Successfully uploaded')
+  writer.write(',forecastDate,prediction,sale,adjustment,lastAdjustment')
+  writer.write('\r\n')
+
+  for (let row of datasetRow) {
+    i = 0
+    for (let cat of catalogs) {
+      let item = _.find(row.catalogItems, {type: cat})
+      if (i++) { writer.write(',') }
+      writer.write(item.externalId + ',' + item.name)
+    }
+    writer.write(',' + row.data.forecastDate + ',' + row.data.prediction + ',' + row.data.sale + ',' + row.data.adjustment + ',' + row.data.lastAdjustment)
+    writer.write('\r\n')
+  }
+
+  await new Promise((resolve, reject) => {
+    writer.close(async () => {
+      fileName = 'datasets/' + fileName
+      let bucket = aws.s3Bucket
+
+      var s3File = {
+        ContentType: 'text/csv',
+        Bucket: bucket,
+        ACL: 'public-read'
+      }
+
+      s3File['Body'] = await fs.readFile(filePath)
+      s3File['Key'] = fileName
+
+      try {
+        var s3 = new awsService.S3({
+          credentials: {
+            accessKeyId: aws.s3AccessKey,
+            secretAccessKey: aws.s3Secret
+          },
+          region: aws.s3Region
+        })
+
+        await s3.putObject(s3File).promise()
+      } catch (e) {
+        console.error(e)
+        reject(e)
+      }
+
+      await AdjustmentDownload.create({
+        organization: dataset.organization,
+        path: fileName,
+        project: dataset.project,
+        dataset: dataset._id
+      })
+      await fs.unlink(filePath)
+      console.log('Successfully uploaded')
+      resolve()
+    })
+  })
+
   return true
 })
 
