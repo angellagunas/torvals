4772c8543baaa1681cf7f1891c6050abe083eab5
Rodman Swanston <swanston.rod@gmail.com>
2018-06-27 00:01:00 -0400

    Custom view

--- app/frontend/pages/organizations/form.js
+++ app/frontend/pages/organizations/form.js
@@ -8,6 +8,7 @@ import {
   TextareaWidget,
   FileWidget
 } from '~base/components/base-form'
+import { shouldRender } from '~base/components/base-form/utils'
 
 const schema = {
   type: 'object',
@@ -24,10 +25,140 @@ const schema = {
 }
 
 const uiSchema = {
-  name: {'ui:widget': TextWidget},
+  'ui:field': 'custom',
+  /*name: {'ui:widget': TextWidget},
   description: {'ui:widget': TextareaWidget, 'ui:rows': 3},
   slug: {'ui:widget': TextWidget, 'ui:disabled': true},
-  profile: {'ui:widget': FileWidget}
+  profile: {'ui:widget': FileWidget}*/
+}
+
+class CustomForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      description: '',
+      slug: '',
+      profile: '',
+      ...props.formData
+    }
+  }
+
+  onChange(name) {
+    return value => {
+      this.setState({ [name]: value })
+      setImmediate(() => this.props.onChange(this.state))
+    }
+  }
+
+  render() {
+    const { name, slug, description, profile, profileUrl } = this.state
+    const profileImg = profile || profileUrl
+
+    return (
+      <div className="columns is-multiline test">
+
+        <div className='column is-12'>
+          <p className='subtitle is-pulled-left'>
+            <strong>Detalle de tu organización</strong>
+          </p>
+          <div className="is-pulled-right">
+            {this.props.children}
+          </div>
+        </div>
+
+        <div className='column is-one-third'>
+          <div className='card'>
+            <div className='card-content'>
+              <div className='columns'>
+                <div className='column'>
+
+                  <center>
+                    {
+                      profileImg && <div
+                        style={{
+                          width: '170px',
+                          height: '170px',
+                          backgroundImage: `url('${profileImg}')`,
+                          backgroundSize: 'cover',
+                          display: 'block',
+                          borderRadius: '100px'
+                        }}
+                      />
+                    }
+                    <div className="form-group field">
+                      <br />
+                      <label className="label">
+                        Sube tu foto de organización
+                      </label>
+                      <br />
+                      <FileWidget
+                        value={profile}
+                        onChange={this.onChange('profile')}
+                        style={{ justifyContent: 'center' }}
+                      />
+                    </div>
+                  </center>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='column'>
+          <div className='card'>
+            <div className='card-content'>
+              <div className='columns'>
+                <div className='column'>
+
+                  <div className="form-group field">
+                    <label className="label">Nombre*</label>
+                    <div className="control">
+                      <TextWidget
+                        required
+                        type='text'
+                        className='input'
+                        value={name}
+                        onChange={this.onChange('name')}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group field">
+                    <label className="label">Descripción</label>
+                    <div className="control">
+                      <TextareaWidget
+                        options={{ rows: 4 }}
+                        type='text'
+                        className='input'
+                        value={description}
+                        onChange={this.onChange('description')}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group field">
+                    <label className="label">Subdominio*</label>
+                    <div className="control">
+                      <TextWidget
+                        required
+                        disabled
+                        type='text'
+                        className='input'
+                        value={slug}
+                        onChange={this.onChange('slug')}
+                      />
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 class OrganizationForm extends Component {
@@ -39,6 +170,10 @@ class OrganizationForm extends Component {
       apiCallMessage: 'is-hidden',
       apiCallErrorMessage: 'is-hidden'
     }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return !shouldRender(this, nextProps, nextState)
   }
 
   errorHandler (e) {}
@@ -103,6 +238,13 @@ class OrganizationForm extends Component {
           onChange={(e) => { this.changeHandler(e) }}
           onSubmit={(e) => { this.submitHandler(e) }}
           onError={(e) => { this.errorHandler(e) }}
+          fields={{
+            custom: props => (
+              <CustomForm {...props}>
+                {this.props.children}
+              </CustomForm>
+            )
+          }}
         >
           <div className={this.state.apiCallMessage}>
             <div className='message-body is-size-7 has-text-centered'>
@@ -115,7 +257,6 @@ class OrganizationForm extends Component {
               {error}
             </div>
           </div>
-          {this.props.children}
         </BaseForm>
       </div>
     )
