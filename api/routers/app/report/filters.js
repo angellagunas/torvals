396e56b1d0daf8c720387c840bf80285827894ad
Cesar <csarg26@gmail.com>
2018-07-05 16:40:10 -0500

    Fix cycle filters

--- api/routers/app/report/filters.js
+++ api/routers/app/report/filters.js
@@ -1,18 +1,47 @@
 const Route = require('lib/router/route')
-const {Project, User, Organization, CatalogItem} = require('models')
+const {Project, User, Organization, CatalogItem, Cycle, Rule} = require('models')
+const moment = require('moment')
 
 module.exports = new Route({
   method: 'get',
-  path: '/filters/',
+  path: '/filters/:uuid',
   handler: async function (ctx) {
     const organization = await Organization.findOne({_id: ctx.state.organization._id})
+    const uuid = ctx.params.uuid
 
-    const projects = await Project.find({
-      isDeleted: false,
-      mainDataset: {$ne: null},
+    const project = await Project.findOne({
+      uuid: uuid,
       organization: organization._id
     })
-    projects.data = projects.map((item) => {
+
+    const rule = await Rule.findOne({
+      organization: organization._id,
+      _id: project.rule
+    })
+
+    const cycles = await Cycle.find({
+      isDeleted: false,
+      'organization': organization._id,
+      rule: rule._id,
+      $or: [
+        {
+          dateStart: {
+            $gte: moment().utc().format('YYYY-MM-DD')
+          }
+        },
+        {
+          dateStart: {
+            $lte: moment().utc().format('YYYY-MM-DD')
+          },
+          dateEnd: {
+            $gte: moment().utc().format('YYYY-MM-DD')
+          }
+        }
+      ]
+
+    }).sort({dateStart: 1}).limit(rule.cyclesAvailable)
+
+    cycles.data = cycles.map((item) => {
       return item.toPublic()
     })
 
@@ -35,7 +64,7 @@ module.exports = new Route({
     })
 
     ctx.body = {
-      projects: projects.data,
+      cycles: cycles.data,
       users: users.data,
       catalogItems: catalogItems.data
     }
