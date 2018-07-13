f0a053bec10ea92fd3c56ea68d6e9b761c6419f2
Luis Lizarraga <siul.al4@gmail.com>
2018-07-13 13:02:10 -0500

    Removing unused queues

--- queues/runner-pio.js
+++ queues/runner-pio.js
@@ -1,17 +1,6 @@
 const createForecast = require('./pio-create-app')
-const loadAppData = require('./pio-load-data')
-const engineBuild = require('./pio-build-engine')
-const engineTrain = require('./pio-train-engine')
-const engineDeploy = require('./pio-deploy-engine')
-const createJson = require('./pio-create-json')
 
-const { each } = require('lodash')
-
-const queues = [createForecast]
-
-each(queues, queue => {
-  queue.run()
-  queue.setCliLogger()
-  queue.setCleanUp()
-})
-console.log(`PIO queues started`)
+createForecast.run()
+createForecast.setCliLogger()
+createForecast.setCleanUp()
+console.log(`PIO master queue started`)
