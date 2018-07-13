de0a08ff042e3cfcf435707b70b96fb846ade3d4
Luis Lizarraga <siul.al4@gmail.com>
2018-07-13 13:00:52 -0500

    Removing unused queues

--- queues/tasks-wrappers/pio-build-engine.js
+++
@@ -1,6 +0,0 @@
-const loadAppData = require('../../tasks/pio-server/engine-build')
-
-module.exports = async function (job) {
-  const a = await loadAppData.run(job.data)
-  return a
-}
--- queues/tasks-wrappers/pio-load-data.js
+++
@@ -1,6 +0,0 @@
-const loadAppData = require('../../tasks/pio-server/load-data')
-
-module.exports = async function (job) {
-  const a = await loadAppData.run(job.data)
-  return a
-}
