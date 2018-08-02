76b4579d8af85d9820a716737f0f38e71ca8fa69
devil-geek <devil.prowler@gmail.com>
2018-08-02 18:40:01 -0500

    Added alert views app/admin

--- app/frontend/pages/landing/land-page.js
+++ app/frontend/pages/landing/land-page.js
@@ -210,7 +210,7 @@ class LandPage extends Component {
                     a tomar mejores decisiones empresariales y a responder de maner más ágil.
                   </h2>
                 </div>
-                <div className='column'>
+                <div className='column has-35-margin-top'>
                   <p>
                     <span className='icon has-text-success'>
                       <i className='fa fa-check' />
