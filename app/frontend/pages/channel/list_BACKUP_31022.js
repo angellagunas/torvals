c644ede2c58b65c743da204f42b8a86eaeea328b
Josue Monroy <jmonroy@grupoabraxas.com>
2018-09-04 18:38:58 -0500

    Merged changes from master/release to dev

---
+++ app/frontend/pages/channel/list_BACKUP_31022.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import Link from '~base/router/link';
+import moment from 'moment';
+import { testRoles } from '~base/tools';
+
+import ListPage from '~base/list-page';
+import { loggedIn, verifyRole } from '~base/middlewares/';
+import CreateChannel from './create';
+
+export default ListPage({
+  path: '/catalogs/channels',
+  title: 'Canales', //TODO: translate
+  icon: 'filter',
+  exact: true,
+  roles:
+    'analyst, orgadmin, admin, consultor-level-2, manager-level-2, consultor-level-3, manager-level-3',
+  validate: [loggedIn, verifyRole],
+  titleSingular: 'Canal', //TODO: translate
+  create: true,
+  createComponent: CreateChannel,
+  export: true,
+  exportRole: 'consultor-level-3',
+  exportUrl: '/app/channels',
+  breadcrumbs: true,
+  breadcrumbConfig: {
+    path: [
+      {
+        path: '/',
+<<<<<<< HEAD
+        label: 'Inicio', //TODO: translate
+        current: false
+      },
+      {
+        path: '/admin/catalogs/channels/',
+        label: 'Canales', //TODO: translate
+        current: true
+      }
+=======
+        label: 'Inicio',
+        current: false,
+      },
+      {
+        path: '/admin/catalogs/channels/',
+        label: 'Canales',
+        current: true,
+      },
+>>>>>>> release
+    ],
+    align: 'left',
+  },
+  canCreate: 'admin, orgadmin, analyst, manager-level-2, manager-level-3',
+  baseUrl: '/app/channels',
+  branchName: 'channels',
+  detailUrl: '/catalogs/channels/',
+  filters: true,
+  schema: {
+    type: 'object',
+    required: [],
+<<<<<<< HEAD
+    properties: { //TODO: translate
+      general: {type: 'text', title: 'Buscar'}
+    }
+=======
+    properties: {
+      general: { type: 'text', title: 'Buscar' },
+    },
+>>>>>>> release
+  },
+  uiSchema: {
+    general: { 'ui:widget': 'SearchFilter' },
+  },
+  getColumns: () => {
+    return [
+      {
+<<<<<<< HEAD
+        'title': 'Nombre', //TODO: translate
+        'property': 'name',
+        'default': 'N/A',
+        'sortable': true,
+        formatter: (row) => {
+          return (
+            <Link to={'/catalogs/channels/' + row.uuid}>
+              {row.name}
+            </Link>
+          )
+        }
+      },
+      {
+        'title': 'Creado', //TODO: translate
+        'property': 'dateCreated',
+        'default': 'N/A',
+        'sortable': true,
+        formatter: (row) => {
+          return (
+            moment.utc(row.dateCreated).local().format('DD/MM/YYYY hh:mm a')
+          )
+        }
+      },
+      {
+        'title': 'Acciones', //TODO: translate
+        formatter: (row) => {
+=======
+        title: 'Nombre',
+        property: 'name',
+        default: 'N/A',
+        sortable: true,
+        formatter: row => {
+          return <Link to={'/catalogs/channels/' + row.uuid}>{row.name}</Link>;
+        },
+      },
+      {
+        title: 'Creado',
+        property: 'dateCreated',
+        default: 'N/A',
+        sortable: true,
+        formatter: row => {
+          return moment
+            .utc(row.dateCreated)
+            .local()
+            .format('DD/MM/YYYY hh:mm a');
+        },
+      },
+      {
+        title: 'Acciones',
+        formatter: row => {
+>>>>>>> release
+          if (testRoles('consultor-level-3, consultor-level-2')) {
+            return (
+              <Link
+                className="button is-primary"
+                to={'/catalogs/channels/' + row.uuid}
+              >
+                <span className="icon is-small" title="Visualizar">
+                  <i className="fa fa-eye" />
+                </span>
+              </Link>
+            );
+          } else {
+            return (
+              <Link
+                className="button is-primary"
+                to={'/catalogs/channels/' + row.uuid}
+              >
+                <span className="icon is-small" title="Editar">
+                  <i className="fa fa-pencil" />
+                </span>
+              </Link>
+            );
+          }
+        },
+      },
+    ];
+  },
+});
---
+++ app/frontend/pages/channel/list_LOCAL_30413.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import Link from '~base/router/link'
+import moment from 'moment'
+import { testRoles } from '~base/tools'
+
+import ListPage from '~base/list-page'
+import {loggedIn, verifyRole} from '~base/middlewares/'
+import CreateChannel from './create'
+
+export default ListPage({
+  path: '/catalogs/channels',
+  title: 'Canales', //TODO: translate
+  icon: 'filter',
+  exact: true,
+  roles: 'analyst, orgadmin, admin, consultor-level-2, manager-level-2, consultor-level-3, manager-level-3',
+  validate: [loggedIn, verifyRole],
+  titleSingular: 'Canal', //TODO: translate
+  create: true,
+  createComponent: CreateChannel,
+  export: true,
+  exportRole: 'consultor-level-3',
+  exportUrl: '/app/channels',
+  breadcrumbs: true,
+  breadcrumbConfig: {
+    path: [
+      {
+        path: '/',
+        label: 'Inicio', //TODO: translate
+        current: false
+      },
+      {
+        path: '/admin/catalogs/channels/',
+        label: 'Canales', //TODO: translate
+        current: true
+      }
+    ],
+    align: 'left'
+  },
+  canCreate: 'admin, orgadmin, analyst, manager-level-2, manager-level-3',
+  baseUrl: '/app/channels',
+  branchName: 'channels',
+  detailUrl: '/catalogs/channels/',
+  filters: true,
+  schema: {
+    type: 'object',
+    required: [],
+    properties: { //TODO: translate
+      general: {type: 'text', title: 'Buscar'}
+    }
+  },
+  uiSchema: {
+    general: {'ui:widget': 'SearchFilter'}
+  },
+  getColumns: () => {
+    return [
+      {
+        'title': 'Nombre', //TODO: translate
+        'property': 'name',
+        'default': 'N/A',
+        'sortable': true,
+        formatter: (row) => {
+          return (
+            <Link to={'/catalogs/channels/' + row.uuid}>
+              {row.name}
+            </Link>
+          )
+        }
+      },
+      {
+        'title': 'Creado', //TODO: translate
+        'property': 'dateCreated',
+        'default': 'N/A',
+        'sortable': true,
+        formatter: (row) => {
+          return (
+            moment.utc(row.dateCreated).local().format('DD/MM/YYYY hh:mm a')
+          )
+        }
+      },
+      {
+        'title': 'Acciones', //TODO: translate
+        formatter: (row) => {
+          if (testRoles('consultor-level-3, consultor-level-2')) {
+            return (
+              <Link className='button is-primary' to={'/catalogs/channels/' + row.uuid}>
+                <span className='icon is-small' title='Visualizar'>
+                  <i className='fa fa-eye' />
+                </span>
+              </Link>
+            )
+          } else {
+            return (
+              <Link className='button is-primary' to={'/catalogs/channels/' + row.uuid}>
+                <span className='icon is-small' title='Editar'>
+                  <i className='fa fa-pencil' />
+                </span>
+              </Link>
+            )
+          }
+        }
+      }
+    ]
+  }
+})
