a88388ba109ec8e519ab247c2cba25f65d95f6f2
Rodman Swanston <swanston.rod@gmail.com>
2018-10-17 19:22:08 -0400

    Mobile view, Period format, Fixed Timer

--- app/frontend/pages/reports/status.js
+++ app/frontend/pages/reports/status.js
@@ -628,7 +628,7 @@ class StatusRepórt extends Component {
             />
           </h2>
         </div>
-        <div className='section columns is-padingless-top'>
+        <div className='section columns is-multiline is-padingless-top'>
           <div className='column'>
             <div className='section level selects is-clearfix'>
               <div className='level-left'>
@@ -702,7 +702,7 @@ class StatusRepórt extends Component {
                       <br />
                     </div>
                     <div className="control">
-                      <button className='button is-primary is-pulled-right'
+                      <button className='button is-primary'
                         disabled={!!this.state.isLoading}
                         onClick={() => this.getDataRows()}
                       >
@@ -759,7 +759,7 @@ class StatusRepórt extends Component {
                   <br />
                 </label>
                 <div className='control'>
-                  <button className='button is-primary is-pulled-right'
+                  <button className='button is-primary'
                     disabled={!!this.state.isLoading}
                     onClick={() => this.download()}
                   >
