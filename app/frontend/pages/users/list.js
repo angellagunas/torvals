b285b0740b9ed40ad09151d981c69099d90c85e5
RodSwanston <swanston.rod@gmail.com>
2018-12-18 14:05:54 -0600

    Add consultor

--- app/frontend/pages/users/list.js
+++ app/frontend/pages/users/list.js
@@ -140,7 +140,6 @@ export default ListPage({
           }
 
           if (currentUser.currentRole.slug === 'consultor-level-3') {
-            console.log('pasee')
             disabledActions = false
           }
 
