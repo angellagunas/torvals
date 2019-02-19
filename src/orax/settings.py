d47bc809c279e5a7d904d1eae76dae11ae93c816
Angel Lagunas <angel.david.lagunas@gmail.com>
2019-02-18 18:38:56 -0600

    (feat:django) config django to use postgres

--- src/orax/settings.py
+++ src/orax/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'orax.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', 'orax'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     },
     'mongo': {
         'NAME': os.environ['MONGO_DB'] if not DEBUG else 'marble-seeds-db',
