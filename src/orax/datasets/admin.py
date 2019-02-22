7cf4fee3f8e96eb54f7a2d66468b628f70a27b53
Angel Lagunas <angel.david.lagunas@gmail.com>
2019-02-21 18:02:02 -0600

    (feat:admin) save datasetrows from dataset

--- src/orax/datasets/admin.py
+++ src/orax/datasets/admin.py
@@ -1,6 +1,45 @@
 """Admin for dataset module."""
+import pandas as pd
+
+from django import forms
 from django.contrib import admin
 from orax.datasets.models import Dataset
+from orax.datasetrows.models import DatasetRow
 
 
-admin.site.register(Dataset)
+class DatasetForm(forms.ModelForm):
+    """Form to admin dataset."""
+
+    class Meta:
+        """Define behavior of class."""
+
+        model = Dataset
+        exclude = []
+
+
+class DatasetAdmin(admin.ModelAdmin):
+    """Admin to manage datasets."""
+
+    def save_model(self, request, obj, form, change):
+        """Save instance."""
+        file = pd.read_csv(obj.file, sep='|', index_col=False)
+        super(DatasetAdmin, self).save_model(request, obj, form, change)
+        dataset = Dataset.objects.latest('created_date')
+
+        for index, row in file.iterrows():
+            DatasetRow.objects.create(
+                dataset_id=dataset.id,
+                organization_id=1,
+                project_id=1,
+                product_id=1,
+                channel_id=1,
+                sale_center_id=1,
+                cycle_id=1,
+                period_id=1,
+                status='unmodified',
+                prediction=row['units'],
+                adjustment=row['units']
+            )
+
+
+admin.site.register(Dataset, DatasetAdmin)
