420e8b5553c437e5c30e958396e096f3828434f7
Angel Lagunas <angel.david.lagunas@gmail.com>
2019-02-22 18:22:56 -0600

    (feat:route) remove unnecesary routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,21 +18,6 @@ const Login = Loadable({
   loading
 });
 
-const Register = Loadable({
-  loader: () => import('./views/Pages/Register'),
-  loading
-});
-
-const Page404 = Loadable({
-  loader: () => import('./views/Pages/Page404'),
-  loading
-});
-
-const Page500 = Loadable({
-  loader: () => import('./views/Pages/Page500'),
-  loading
-});
-
 class App extends Component {
 
   render() {
@@ -40,9 +25,6 @@ class App extends Component {
       <HashRouter>
           <Switch>
             <Route exact path="/login" name="Login Page" component={Login} />
-            <Route exact path="/register" name="Register Page" component={Register} />
-            <Route exact path="/404" name="Page 404" component={Page404} />
-            <Route exact path="/500" name="Page 500" component={Page500} />
             <Route path="/" name="Home" component={DefaultLayout} />
           </Switch>
       </HashRouter>
