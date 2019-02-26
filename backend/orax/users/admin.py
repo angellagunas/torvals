3105ed8377bebc39a0d7be4c1589cda4be12323c
Angel Lagunas <angel.david.lagunas@gmail.com>
2019-02-25 23:15:06 -0600

    (fix:admin) set password with hash

--- backend/orax/users/admin.py
+++ backend/orax/users/admin.py
@@ -1,6 +1,12 @@
 """Admin for users module."""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from orax.users.models import User
 
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+	"""Admin for user."""
+    pass
+
+
+admin.site.register(User, CustomUserAdmin)
