05ae5b115f94a268b8d9ad32a660774303521d9a
Angel Lagunas <angel.david.lagunas@gmail.com>
2019-04-25 19:11:48 -0500

    (feat:admin) add celery on admin

--- backend/app/utils/tasks.py
+++ backend/app/utils/tasks.py
@@ -1,3 +1,5 @@
+"""Celery tasks."""
+
 from app.settings import SLACKBOT_TOKEN, NOTIFICATIONS_CHANNEL
 from slacker import Slacker
 from celery import shared_task
@@ -5,7 +7,7 @@ from celery import shared_task
 
 @shared_task
 def send_slack_notifications(message):
-    """ Function that post a message to the channel of choice """
+    """Function that post a message to the channel of choice."""
     slack = Slacker(SLACKBOT_TOKEN)
     channel = NOTIFICATIONS_CHANNEL
     message = (
