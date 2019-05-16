9342ac8be2ffaca208d9020e8dc0d26f773bad9e
Angel Lagunas <angel.david.lagunas@gmail.com>
2019-05-16 18:11:15 -0500

    (feat:tasks) add cron to send email to users

--- backend/app/utils/plugins/tasks.py
+++ backend/app/utils/plugins/tasks.py
@@ -1,7 +1,7 @@
 """Manage tasks of all projects."""
 from celery import shared_task
 
-from app.utils.plugins.utils import GloboUtils
+from app.utils.plugins.utils import BarcelUtils, GloboUtils
 from app.utils.tasks import send_slack_notifications
 
 
@@ -12,5 +12,13 @@ def run_globo_process():
     try:
         GloboUtils().run()
     except Exception as e:
-        send_slack_notifications.apply_async(("ElGlobo process\nError: {}".format(e),))
+        send_slack_notifications.apply_async(
+            ("ElGlobo process\nError: {}".format(e),)
+        )
     send_slack_notifications.apply_async(("ElGlobo process has finished.",))
+
+
+@shared_task
+def send_order_to_dispatcher(dispatcher_email, receivers):
+    """Send order to user without adjustment."""
+    BarcelUtils().send_order_to_dispatcher(dispatcher_email, receivers)
